669c7683295bf88c6102579068d432fb9ff1e8c6
mp1220 <milesphillips@comcast.net>
2025-11-07 23:31:17 -0500

    Delete trader_v1 directory

--- trader_v1/app/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Application package initialization for the trading bot."""
-
-# Expose key submodules for convenient imports when running as a package.
-# This keeps imports like `from app import config` working consistently.
